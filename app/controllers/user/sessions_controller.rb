5bd0dafea1e8aaa6c71e855941bf7714147f09ca
ta-ito <ta-ito@users.noreply.github.com>
2016-05-03 23:02:53 +0900

    Change namespece

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,22 +1,20 @@
-class Users::SessionsController < Devise::SessionsController
+class User::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
-  def create
-    flash[:notice] = "login"
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # DELETE /resource/sign_out
-  def destroy
-    flash[:notice] = "logout"
-    super
-  end
+  # def destroy
+  #   super
+  # end
 
   protected
 
